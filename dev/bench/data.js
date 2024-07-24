fdc0489a6a25fb835563979aef833e1381012ef5
github-action-benchmark <github@users.noreply.github.com>
2024-07-24 10:30:04 +0000

    add Movet Middleware Benchmark Project Perception Pipeline Benchmark (googlecpp) benchmark result for 563d83b4be6eb932037be92f99f1e3efa2ef1400

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1720737405593,
+  "lastUpdate": 1721817004421,
   "repoUrl": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark",
   "entries": {
     "Movet Middleware Benchmark Project Perception Pipeline Benchmark": [
@@ -30,6 +30,36 @@ window.BENCHMARK_DATA = {
             "value": 257076855860.00003,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 1715919191 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cihataltiparmak1@gmail.com",
+            "name": "CihatAltiparmak",
+            "username": "CihatAltiparmak"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "563d83b4be6eb932037be92f99f1e3efa2ef1400",
+          "message": "Merge pull request #6 from CihatAltiparmak/feature/custom_github_actions\n\nFeature/custom GitHub actions",
+          "timestamp": "2024-07-24T10:24:14Z",
+          "tree_id": "ead7814622935ebd6dce7f538ac938be467364c4",
+          "url": "https://github.com/CihatAltiparmak/moveit_middleware_benchmark/commit/563d83b4be6eb932037be92f99f1e3efa2ef1400"
+        },
+        "date": 1721817004003,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ScenarioPerceptionPipelineFixture/test_scenario_perception_pipeline",
+            "value": 254523055801.00003,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 1679029366 ns\nthreads: 1"
           }
         ]
       }
